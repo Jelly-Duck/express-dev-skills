d37d58b2b955e37c96046968619b909af05e7cee
Felix <andrewchris99@yahoo.com>
2024-02-27 13:21:36 -0500

    Complete

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+var methodOverride = require("method-override"); // <--- Brings in the method override module
 
 var indexRouter = require("./routes/index");
 var skilluhzRouter = require("./routes/skilluhz");
@@ -18,6 +19,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+app.use(methodOverride("_method"));
 
 app.use("/", indexRouter);
 app.use("/skillz", skilluhzRouter);
